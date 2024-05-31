776a21d90ab4613773cb57ff6d199e0d848f6ea4
Ignacio <ignacio.martinez@ucb.edu.bo>
2024-05-31 15:54:22 -0400

    prueba para obtener detalles del producto 0

--- features/VisualizacionProductos.feature
+++ features/VisualizacionProductos.feature
@@ -1,3 +1,10 @@
-Feature: As a internet user
-         I want to use the google search engine
-         so I test that it works correctly
+Feature: Testing GMO Post
+  As an internet user
+  I want to interact with the demo GMO Post website
+  So I can verify its functionality
+
+Scenario: Clicking buttons and verifying text
+  Given I am on the GMO Post homepage
+  When I click the initial button
+  And I click the second link
+  Then I should see the expected text
--- features/step_definitions/VisualizacionProductos.rb
+++ features/step_definitions/VisualizacionProductos.rb
@@ -1,3 +1,17 @@
-Given('I am on the Google homepage') do
-    visit('/')
+Given('I am on the GMO Post homepage') do
+    visit 'https://demo.borland.com/gmopost/'
+  end
+
+When('I click the initial button') do
+find('body > form > div:nth-child(1) > center > table > tbody > tr > td:nth-child(1) > input[type=button]').click
+end
+
+When('I click the second link') do
+find('body > form > table > tbody > tr:nth-child(2) > td > div > center > table > tbody > tr:nth-child(2) > td:nth-child(2) > a > strong').click
+end
+
+Then('I should see the expected text') do
+expected_text = find('body > p:nth-child(4) > font > em').text
+puts "ONLY FOR TEST PURPOSES: #{expected_text}"
+expect(expected_text).to eq('Our best 3 person backpack dome tent')
 end
