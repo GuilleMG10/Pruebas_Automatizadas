86f8f65f9b331e9f7ecdb514b0450f85677ca126
Guillermo <guichi.maldo@gmail.com>
2024-06-02 22:42:03 -0400

    Ya verifica que se pueda anadir productos y calcula el total

--- features/RealizarCompra.feature
+++ features/RealizarCompra.feature
@@ -1,3 +1,32 @@
-Feature: As a internet user
-         I want to use the google search engine
-         so I test that it works correctly
+Feature: Buy Products
+  As a client
+  I want to select products to buy
+  So that I can verify that the purchase process works correctly and the total amount is accurate
+
+  Background:
+   Given I am on the GMO 
+   When I click the button Enter GMO OnLine button
+
+
+
+  Scenario: Select one product
+   
+    When I update the quantity of "3 Person Dome Tent" to "5"
+    Then the quantity for "3 Person Dome Tent" should be "5"
+@cc
+    Scenario Outline: Select Products
+
+    When I update the quantity of "<product_name>" to "<quantity>"
+    And Click on the Place and orden Button
+    Then the TotalAmount should be "<Total>"
+
+    Examples:
+      | product_name              | quantity |Total
+    | 3 Person Dome Tent   | 2        |$ 634.98
+    | External Frame Backpack | 1      |$ 193.95
+    | Glacier Sun Glasses  | 3        |$ 219.17
+    | Padded Socks         | 4        |$ 88.96
+    | Hiking Boots         | 1        |$ 120.4
+    | Back Country Shorts  | 2        |$ 57.39
+
+ 
--- features/step_definitions/RealizarCompra.rb
+++ features/step_definitions/RealizarCompra.rb
@@ -1,3 +1,43 @@
-Given('I am on the Google homepage') do
-    visit('/')
+Given('I am on the GMO') do
+    visit 'https://demo.borland.com/gmopost/'
+end
+
+When('I click the button Enter GMO OnLine button') do
+    find('body > form > div:nth-child(1) > center > table > tbody > tr > td:nth-child(1) > input[type=button]').click
+end
+
+When('I update the quantity of {string} to {string}') do |product_name, new_quantity|
+    counter = 2
+    while counter < 7
+    puts "Iteración: #{counter}"
+
+    row_selector = "body > form > table > tbody > tr:nth-child(#{counter}) > td > div > center > table > tbody > tr"
+    row = find(:css, row_selector, text: product_name)
+        if row.visible?
+            last_column_cell = row.find('td:last-child input[type="text"]')
+            last_column_cell.click
+            last_column_cell.set(new_quantity)
+            break
+        end
+        
+        counter += 1
+    end
+end
+  
+  
+
+When('Click on the Place and orden Button') do  
+    find('body > form > table > tbody > tr:nth-child(3) > td > div > center > table > tbody > tr > td > p > input[type=submit]:nth-child(2)').click
+    sleep(2)
+end
+
+Then('the TotalAmount should be {string}') do |expected_total|
+
+    actual_total = find(:xpath, "/html/body/form/table/tbody/tr[1]/td/div/center/table/tbody/tr[6]/td[2]").text
+
+    if actual_total == expected_total
+        puts "El valor del total es correcto: #{actual_total}"
+    else
+        puts "El valor del total es incorrecto. Se esperaba #{expected_total} pero se encontró #{actual_total}"
+    end
 end
