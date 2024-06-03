5769087f1281f96f91fa6f2480d66a83e67b293d
Ignacio <ignacio.martinez@ucb.edu.bo>
2024-06-03 15:38:37 -0400

    happy path de bill information

--- features/BillInformation.feature
+++ features/BillInformation.feature
@@ -1,5 +1,26 @@
-Feature: Bill Information
-    As an internet user
-    I want to place an order in the demo GMO Post website
-    so I test bill information works correctly
+Feature: Bill Information Form
+  As a user
+  I want to fill out the Bill Information form
+  So I can complete the billing and shipping details
 
+Background:
+    Given I am on the GMO homepage
+    And I enter to product catalog
+    And I place an order
+    And I proceed with order
+
+Scenario: Fill out Bill To information
+    When I fill in the Bill To section with the following details:
+        | Name    | Pepe Perez    |
+        | Address | 123 Main St   |
+        | City    | ciudad        |
+        | State   | estado        |
+        | Zip     | 12345         |
+        | Phone   | 555-555-5555  |
+        | E-mail  | john@example.com |
+    And I select "Visa" from the Credit Card combo box
+    And I enter "4111111111111111" as the Card Number
+    And I enter "12/24" as the Expiration date
+    And I check the Same as Bill To checkbox
+    And I click Place the Order button
+    Then the order should be accepted
--- features/step_definitions/VisualizacionProductos.rb
+++ features/step_definitions/VisualizacionProductos.rb
@@ -3,17 +3,17 @@ Given('I am on the GMO Post homepage') do
   end
 
 When('I click Enter GMO OnLine button') do
-find('body > form > div:nth-child(1) > center > table > tbody > tr > td:nth-child(1) > input[type=button]').click
+  find('body > form > div:nth-child(1) > center > table > tbody > tr > td:nth-child(1) > input[type=button]').click
 end
 
 When('I click 3 Person Dome Tent product') do
-find('body > form > table > tbody > tr:nth-child(2) > td > div > center > table > tbody > tr:nth-child(2) > td:nth-child(2) > a > strong').click
+  find('body > form > table > tbody > tr:nth-child(2) > td > div > center > table > tbody > tr:nth-child(2) > td:nth-child(2) > a > strong').click
 end
 
 Then('I should see details of selected product') do
-expected_text = find('body > p:nth-child(4) > font > em').text
-puts "ONLY FOR TEST PURPOSES: #{expected_text}"
-expect(expected_text).to eq('Our best 3 person backpack dome tent')
+  expected_text = find('body > p:nth-child(4) > font > em').text
+  puts "ONLY FOR TEST PURPOSES: #{expected_text}"
+  expect(expected_text).to eq('Our best 3 person backpack dome tent')
 end
 
 When('I click {string} product') do |product_name|
